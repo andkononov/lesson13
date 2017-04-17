6233dde152755325899dd2fa2d8a07a28b855407
maksim_burakouski <maksim_burakouski@epam.com>
2017-04-17 08:39:20 +0300

    lesson 13

--- productionsite.pp
+++ productionsite.pp
@@ -1,21 +1,30 @@
-node 'agent2.bm' {
+node 'server.bm' {
+  class {'puppetdb':}
+  class { 'puppetdb::master::config': }
+  class {'::puppetexplorer':
+vhost_options => {
+rewrites => [ { rewrite_rule => ['^/api/metrics/v1/mbeans/puppetlabs.puppetdb.query.population:type=default,name=(.*)$ https://%{HTTP_HOST}/api/metrics/v1/mbeans/puppetlabs.puppetdb.population:name=$1 [R=301,L]'] } ] }
+}
+  }
+
+node 'agent1.bm' {
   class {'::mysql::server':
     root_password    => 'password',
   }
-  class { 'nginx': }
-  mysql_database {'prod_mdb':
+  mysql_database {'test_mdb':
     ensure  => present,
     charset => 'utf8',
   }
-  mysql_user { 'prod_user@localhost':
+  mysql_user { 'test_user@localhost':
     ensure => present,
     password_hash => mysql_password('test'),
   }
-  mysql_grant {'prod_user@localhost/prod_mdb.*':
+  mysql_grant {'test_user@localhost/test_mdb.*':
     ensure     => present,
     options    => ['GRANT'],
     privileges => ['ALL'],
-    table      => 'prod_mdb.*',
-    user       => 'prod_user@localhost',
+    table      => 'test_mdb.*',
+    user       => 'test_user@localhost',
   }
 }
+
